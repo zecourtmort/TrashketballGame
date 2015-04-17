49dee327b2574d511c21a08bcf2ac5c288621d15
Courtney Mort <cmort@mines.edu>
2015-04-17 00:10:48 -0600

    tests written

--- src/Game/Math.java
+++ src/Game/Math.java
@@ -1,15 +1,26 @@
 package Game;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
 
 
 public class Math {
 	private int height;
 	private int length;
 	private static double GRAVITY = 9.81;
+	private static int VELOCITY = 13;
+	private ArrayList<Point> points;
 	public static int calcTarget(int height, int distance){
 		return 0;
 	}
+	public void calcPoints(int distance){
+		
+	}
 	public void drawAngle(Graphics g, int angle){
 		
+	}
+	public static ArrayList<Point> getPoints() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- src/Test/TrashketTests.java
+++ src/Test/TrashketTests.java
@@ -33,17 +33,34 @@ public class TrashketTests {
 	public void configTest() throws FileNotFoundException{
 		int testHeight = game.getLevels().get(0).getTrashHeight();
 		int testDistance = game.getLevels().get(0).getTrashDistance();
-		assertEquals(testHeight, 0);
+		assertEquals(testHeight, 2);
 		assertEquals(testDistance, 15);
+		testHeight = game.getLevels().get(1).getTrashHeight();
+		testDistance = game.getLevels().get(1).getTrashDistance();
+		assertEquals(testHeight, 22);
+		assertEquals(testDistance, 33);
+		testHeight = game.getLevels().get(2).getTrashHeight();
+		testDistance = game.getLevels().get(2).getTrashDistance();
+		assertEquals(testHeight, 8);
+		assertEquals(testDistance, 3);
 	}
 	@Test
 	public void targetTest(){
 		assertEquals(Math.calcTarget(game.getLevels().get(0).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 45);
+		assertEquals(Math.calcTarget(game.getLevels().get(1).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 60);
+		assertEquals(Math.calcTarget(game.getLevels().get(2).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 70);
 	}
-	
+	@Test
+	public void pointsTest(){
+		int testDistance = game.getLevels().get(0).getTrashDistance();
+		assertEquals(Math.getPoints().size(), testDistance+1);
+		assertEquals(Math.getPoints().get(0).getX(),0,0);
+		assertEquals(Math.getPoints().get(0).getX(),testDistance+1,0);
+	}
 	@Test
 	public void solutionTest(){
 		assertTrue(game.checkSolution(45));
+		assertTrue(game.checkSolution(60));
 	}
 
 }
