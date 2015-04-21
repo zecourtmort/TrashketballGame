8dc4fa327943d38c44739c35d6b8fd5413604998
Courtney Mort <cmort@mines.edu>
2015-04-21 10:59:37 -0600

    Tests work

--- src/Game/TrashketballGame.java
+++ src/Game/TrashketballGame.java
@@ -1,21 +1,43 @@
 package Game;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class TrashketballGame {
-	private ArrayList<Level> levels;
+	private static ArrayList<Level> levels = new ArrayList<Level>();
 	private int currentSolution;
 	private Level currentLevel;
-	public TrashketballGame(String configFileName){
-		
+	private String configFileName;
+	private int lifeCounter = 3;
+	public TrashketballGame(String configFileName) throws FileNotFoundException{
+		this.configFileName = configFileName;
+		loadConfigFiles();
 	}
-	public void loadConfigFiles(){
-		
+	public void loadConfigFiles() throws FileNotFoundException{
+		FileReader file = new FileReader(configFileName);
+		Scanner in = new Scanner(file);
+		while(in.hasNextLine()){
+			int height = in.nextInt();
+			int distance = in.nextInt();
+			Level temp = new Level(height, distance);
+			levels.add(temp);
+		}
 	}
 	public boolean checkSolution(int angle){
-		return false;
+		return (currentSolution == angle);
 	}
 	public ArrayList<Level> getLevels(){
 		return levels;
+	}
+	public void setCurrentSolution(int angle){
+		this.currentSolution = angle;
+	}
+	public static void main(String[] args){
+		int i = 0;
+		while ( i < levels.size()){
+			
+		}
 	}
 }
--- src/Test/TrashketTests.java
+++ src/Test/TrashketTests.java
@@ -11,19 +11,20 @@ import java.io.FileNotFoundException;
 
 
 
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Game.Physics;
 import Game.TrashketballGame;
 import Game.Level;
-import Game.Math;
 import Game.Quiz;
 
 public class TrashketTests {
 	private static TrashketballGame game;
 	@BeforeClass
-	public void setups(){
-		game = new TrashketballGame("configFile.txt");
+	public static void setups() throws FileNotFoundException{
+		game = new TrashketballGame("LevelConfig.txt");
 	}
 	@Test (expected = FileNotFoundException.class)
 	public void badConfigTest() throws FileNotFoundException{
@@ -37,29 +38,32 @@ public class TrashketTests {
 		assertEquals(testDistance, 15);
 		testHeight = game.getLevels().get(1).getTrashHeight();
 		testDistance = game.getLevels().get(1).getTrashDistance();
-		assertEquals(testHeight, 22);
-		assertEquals(testDistance, 33);
+		assertEquals(testHeight, 6);
+		assertEquals(testDistance, 9);
 		testHeight = game.getLevels().get(2).getTrashHeight();
 		testDistance = game.getLevels().get(2).getTrashDistance();
-		assertEquals(testHeight, 8);
+		assertEquals(testHeight, 6);
 		assertEquals(testDistance, 3);
 	}
 	@Test
 	public void targetTest(){
-		assertEquals(Math.calcTarget(game.getLevels().get(0).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 45);
-		assertEquals(Math.calcTarget(game.getLevels().get(1).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 60);
-		assertEquals(Math.calcTarget(game.getLevels().get(2).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 70);
+		assertEquals(Physics.calcTarget(game.getLevels().get(0).getTrashHeight(), game.getLevels().get(0).getTrashDistance()), 45);
+		assertEquals(Physics.calcTarget(game.getLevels().get(1).getTrashHeight(), game.getLevels().get(1).getTrashDistance()), 60, 3);
+		assertEquals(Physics.calcTarget(game.getLevels().get(2).getTrashHeight(), game.getLevels().get(2).getTrashDistance()), 70, 3);
 	}
 	@Test
 	public void pointsTest(){
 		int testDistance = game.getLevels().get(0).getTrashDistance();
-		assertEquals(Math.getPoints().size(), testDistance+1);
-		assertEquals(Math.getPoints().get(0).getX(),0,0);
-		assertEquals(Math.getPoints().get(0).getX(),testDistance+1,0);
+		Physics.calcPoints(testDistance, 51);
+		assertEquals(Physics.getPoints().size(), testDistance+1);
+		assertEquals(Physics.getPoints().get(0).getX(),0,0);
+		assertEquals(Physics.getPoints().get(Physics.getPoints().size()-1).getX(),testDistance,0);
 	}
 	@Test
 	public void solutionTest(){
+		game.setCurrentSolution(45);
 		assertTrue(game.checkSolution(45));
+		game.setCurrentSolution(60);
 		assertTrue(game.checkSolution(60));
 	}
 
